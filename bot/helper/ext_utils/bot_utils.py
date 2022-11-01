2accdf25ea4e23c64af44ba167fbc8e11efd479e
Hotmoon <aravind1333.av@gmail.com>
2022-11-01 21:09:56 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -178,10 +178,10 @@ def get_readable_message():
                 globals()['PAGE_NO'] -= 1
         for index, download in enumerate(list(download_dict.values())[COUNT:], start=1):
             msg += f"<b>╭</b> <code>{escape(str(download.name()))}</code>"
-            msg += f"\n<b>├{download.status()}</b>"
+            msg += f"\n<b>├ {download.status()}...</b>"
             if download.status() not in [MirrorStatus.STATUS_SEEDING, MirrorStatus.STATUS_SPLITTING]:
                 if EMOJI_THEME is True:
-                    msg += f"\n<b>├</b> {get_progress_bar_string(download)}  {download.progress()}"
+                    msg += f"\n<b>├</b> {get_progress_bar_string(download)} {download.progress()}"
                     msg += f"\n<b>├🔄 Process:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                     msg += f"\n<b>├⚡ Speed:</b> {download.speed()}"
                     msg += f"\n<b>├⏳ ETA:</b> {download.eta()}"
@@ -189,7 +189,7 @@ def get_readable_message():
                     msg += f"\n<b>├⛓️ Engine :</b> {download.eng()}"
 
                 else:
-                    msg += f"\n<b>├</b> {get_progress_bar_string(download)}{download.progress()}"
+                    msg += f"\n<b>├</b> {get_progress_bar_string(download)} {download.progress()}"
                     msg += f"\n<b>├ Process:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                     msg += f"\n<b>├ Speed:</b> {download.speed()}"
                     msg += f"\n<b>├ ETA:</b> {download.eta()}"
